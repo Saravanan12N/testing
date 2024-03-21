fbc1e6dcf4dc347bae26a9922683a617d613d689
Sarvanan N <nsaravanan1204@gmail.com>
2024-03-21 23:28:27 +0530

    Advanced React

---
+++ src/components/App.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { createStore } from 'redux';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
+
+// const reducer = (state = { count: 0 }, action) => {
+//     switch (action.type) {
+//         case 'INCREMENT':
+//             return {
+//                 count: state.count + 1
+//             }
+//         case 'DECREMENT':
+//             return {
+//                 count: state.count - 1
+//             }
+//         case 'RESET':
+//             return {
+//                 count: 0
+//             }
+//         default:
+//             return state
+
+//     }
+// }
+
+// const store = createStore(reducer);
+
+// //Increment
+// // store.subscribe(() => console.log(store.getState()));
+// store.dispatch({
+//     type: 'INCREMENT'
+// });
+
+// store.dispatch({
+//     type: 'INCREMENT'
+// });
+// store.dispatch({
+//     type: 'INCREMENT'
+// });
+// //Decrement 
+
+// store.dispatch({
+//     type: 'DECREMENT'
+// })
+
+// store.dispatch({
+//     type: 'RESET'
+// })
+
+const store = configureStore();
+store.dispatch(addExpense({ description: 'Water Bill' }));
+store.dispatch(addExpense({ description: 'Gas Bill' }));
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+
+console.log(visibleExpenses);
+export default () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
